04ecd9421907b19f10692c6b2cfcf7d6b7dcacb4
camille56 <camille.david.dev@proton.me>
2024-12-13 16:12:12 +0100

    fichier de migration manquant pour le commit précédent

--- migrations/Version20241213143837.php
+++ migrations/Version20241213143837.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241212152651 extends AbstractMigration
+final class Version20241213143837 extends AbstractMigration
 {
     public function getDescription(): string
     {
