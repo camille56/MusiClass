412929e42e07bf75af4442a0fc9694042267bec8
camille56 <camille.david.dev@proton.me>
2024-12-18 17:59:11 +0100

    Création de l'enregistrement de video pour un cours dans un fichier upload défini dans le fichier services.yaml

--- src/Controller/Admin/CoursController.php
+++ src/Controller/Admin/CoursController.php
@@ -6,28 +6,26 @@ use App\Entity\Cours;
 use App\Form\CoursType;
 use App\Repository\CoursRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 
 #[IsGranted("IS_AUTHENTICATED")]
 #[Route('/admin/cours')]
 class CoursController extends AbstractController
 {
-//    #[Route('', name: 'app_admin_cours')]
-//    public function index(): Response
-//    {
-//        return $this->render('admin/cours/index.html.twig', [
-//            'controller_name' => 'CoursController',
-//        ]);
-//    }
-
+    /**
+     * @throws Exception
+     */
     #[Route('/cours/{id}',name: 'app_admin_cours_editcours',requirements: ['id'=>'\d+'], methods: ['GET', 'POST'],)]
     #[Route('', name: 'app_admin_cours_newCours')]
-    public function newCours(Request $request, EntityManagerInterface $manager, ?Cours $cours): Response{
+    public function newCours(Request $request, EntityManagerInterface $manager, ?Cours $cours,SluggerInterface $slugger): Response{
 
         $cours ??= new Cours();
 
@@ -36,9 +34,48 @@ class CoursController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
+            //Gestion de la date de création
             $dateCreation = new \DateTimeImmutable();
             $cours->setDateCreation($dateCreation);
 
+            //Gestion de la vidéo
+            $fichierVideo=$form->get('video')->getData();
+
+            if ($fichierVideo){
+
+                //suppression de l'ancienne vidéo si elle existe
+                if ($cours->getVideo()){
+                    $ancienneVideo =$this->getParameter('dossier_uploads').'/'.$cours->getVideo();
+                    if (file_exists($ancienneVideo)){
+                        unlink($ancienneVideo);
+                    }
+                }
+
+                //génération du nom de la vidéo avec in identifiant unique
+
+                $nomBaseVideo=pathinfo($fichierVideo->getClientOriginalName(),PATHINFO_FILENAME);//permet d'avoir le nom du fichier téléchargé.
+                $nomBaseSecuriseVideo=$slugger->slug($nomBaseVideo);//permet de transformer le string pour être compatible avec le système de fichier et urls
+                $extensionVideo=$fichierVideo->guessExtension(); //permet d'avoir l'extension du fichier en fonction de son type mime
+                $nomVideo=$nomBaseSecuriseVideo.'-'.uniqid().'.'.$extensionVideo; //uniqid() génère un string unique comme "6762fe9aee198". Basé sur l'heure
+
+
+                //Déplacement du fichier dans le dossier des uploads
+                try {
+                    $fichierVideo->move(
+                        $this->getParameter('dossier_uploads'), //le dossier upload est défini dans services.yaml
+                        $nomVideo
+                    );
+
+                    //Définition du nom de la vidéo en bdd
+                    $cours->setVideo($nomVideo);
+
+                }catch(FileException $e){
+                    throw new Exception('Erreur lors du téléchargement de la vidéo.');
+                }
+
+            }
+
+            //envoi BDD
             $manager->persist($cours);
             $manager->flush();
 
--- src/Form/CoursType.php
+++ src/Form/CoursType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Doctrine\DBAL\Types\DateImmutableType;
 use Doctrine\DBAL\Types\IntegerType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,9 +16,24 @@ class CoursType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('video', TextType::class)
-            ->add('texte', TextareaType::class)
             ->add('titre', TextType::class)
+            ->add('texte', TextareaType::class)
+            ->add('video', FileType::class, [
+                    'label' => 'Vidéo (MP4 uniquement)',
+                    'mapped' => false,
+                    'required' => false,
+                    'constraints' => [
+                        new \Symfony\Component\Validator\Constraints\File([
+                                'maxSize' => '50M', // Limite de taille
+                                'mimeTypes' => [
+                                    'video/mp4',
+                                ],
+                                'mimeTypesMessage' => 'Veuillez uploader une vidéo au format MP4 uniquement',
+                            ]
+                        )
+                    ]
+                ]
+            )
         ;
     }
 
